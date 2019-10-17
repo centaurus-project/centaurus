18eb53d74f7ccc24603a92ad174b9a90d0c4d048
hawthorne-abendsen <49230725+hawthorne-abendsen@users.noreply.github.com>
2019-10-17 17:54:34 +0300

    Implemented #18 (Auditor Heartbeat message implementation)

--- Centaurus.Models/Settings/Heartbeat.cs
+++ Centaurus.Models/Settings/Heartbeat.cs
@@ -7,11 +7,5 @@ namespace Centaurus.Models
     public class Heartbeat : Message
     {
         public override MessageTypes MessageType => MessageTypes.Heartbeat;
-
-        public static Heartbeat DecodeXdr(XdrReader stream)
-        {
-            //TODO: discuss if wee need to sign and verify some data here
-            return new Heartbeat();
-        }
     }
 }
