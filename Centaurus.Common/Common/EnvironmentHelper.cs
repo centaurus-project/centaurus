98bf4e34b3229fbb7efff78b37aef5ada8a07dee
hawthorne-abendsen <49230725+hawthorne-abendsen@users.noreply.github.com>
2020-05-27 21:08:11 +0300

    Continues integration implementation

---
+++ Centaurus.Common/Common/EnvironmentHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Centaurus
+{
+    public static class EnvironmentHelper
+    {
+        public static string IsTestEnvVarName => "IsTest";
+
+        public static void SetTestEnvironmentVariable(bool value = true)
+        {
+            Environment.SetEnvironmentVariable(EnvironmentHelper.IsTestEnvVarName, value.ToString());
+        }
+
+        public static bool IsTest => bool.TryParse(Environment.GetEnvironmentVariable(IsTestEnvVarName), out bool isTest) && isTest;
+    }
+}
--- Centaurus.Test.Domain/AuditorQuantumHandlerTests.cs
+++ Centaurus.Test.Domain/AuditorQuantumHandlerTests.cs
@@ -14,6 +14,7 @@ namespace Centaurus.Test
         [SetUp]
         public void Setup()
         {
+            EnvironmentHelper.SetTestEnvironmentVariable();
             GlobalInitHelper.DefaultAuditorSetup();
         }
 
